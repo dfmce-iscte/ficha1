e01c19ae078fee9f274a70a0f2b275ceddb42e26
dfmce-iscte <dfmce@iscte-iul.pt>
2022-03-03 15:47:34 +0000

    As vjd svjksdv msnd vjfbkjsdvns vjsdbvkjsdnca jkasnfliewjfoen

--- teste3/src/com/company/Main.java
+++ teste3/src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("NO BIMA");
         System.out.println("A seguir sou eu");
     }
 }
